9770f74463eb32b35d3c7f45f85acffe30b9b9b6
Robert Klim <kontakt.robert.klim@gmail.com>
2018-10-08 09:28:19 +0200

    Generate and draw ship

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 const FPS = 30;
-
+const SHIP_SIZE = 30;
 /* @type {HTMLCanvasElement} */
 let canv = document.getElementById("gameCanvas");
 let ctx = canv.getContext("2d");
@@ -7,8 +7,15 @@ let ctx = canv.getContext("2d");
 // game loop
 setInterval(update, 1000 / FPS);
 
+// ship
+let ship = new Ship(canv.width / 2, canv.height - SHIP_SIZE / 2, SHIP_SIZE);
+
 function update() {
     // draw space
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canv.width, canv.height);
+
+    // draw ship
+    ship.draw();
+
 }
---
+++ js/ship.js
@@ -0,0 +1,31 @@
+class Ship {
+    constructor(x, y, size = 30, a = 90 / 180 * Math.PI, color = "white") {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.r = this.size / 2;
+        this.a = a;
+        this.color = color;
+    }
+
+    draw() {
+        ctx.strokeStyle = this.color;
+        ctx.lineWidth = this.size / 20;
+        ctx.beginPath();
+        ctx.moveTo( // nose of the ship
+            this.x + 4 / 3 * this.r * Math.cos(this.a),
+            this.y - 4 / 3 * this.r * Math.sin(this.a)
+        );
+        ctx.lineTo( // rear left
+            this.x - this.r * (2 / 3 * Math.cos(this.a) + Math.sin(this.a)),
+            this.y + this.r * (2 / 3 * Math.sin(this.a) - Math.cos(this.a))
+        );
+        ctx.lineTo( // rear right
+            this.x - this.r * (2 / 3 * Math.cos(this.a) - Math.sin(this.a)),
+            this.y + this.r * (2 / 3 * Math.sin(this.a) + Math.cos(this.a))
+        );
+        ctx.closePath();
+        ctx.stroke();
+    }
+
+}
